a58b57bb556c2b62b6ecd33799ccfec122b7c1d2
Rebeca Ouriques <ouriques.rebecah@gmail.com>
2024-08-07 15:53:17 -0300

    alteração da app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import Footer from './Components/Footer/Footer'
 import Menu from './Components/Menu/Menu'
 import Home from './Pages/Home/Home'
-import Produto from './Components/Produto/Produto'
-
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
