0bb3d6a68ce2a18990fe6846ec5853d8b6c70023
Camila Andrade <camila.rda@outlook.com>
2024-08-13 17:30:37 -0300

    fix login

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -16,6 +16,12 @@ function Login() {
 
   const {isLoading} = useContext(AuthContext) 
 
+useEffect(() => {
+    if (usuario.token !== "") {
+        navigate('/home')
+    }
+}, [usuario])
+
 function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
   setUsuarioLogin({
       ...usuarioLogin,
