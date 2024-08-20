c101d5a54cfc718e4d1b04874f9d86da09ce68c0
GabrielSantanaDaSilva <topsstdeq@gmail.com>
2024-08-20 11:18:45 -0300

    Feat: Cards novos

--- src/Components/Categoria/cardCategoria/CardCategoria.tsx
+++ src/Components/Categoria/cardCategoria/CardCategoria.tsx
@@ -8,26 +8,47 @@ interface CardCategoriaProps {
 
 function CardCategoria({categoria}: CardCategoriaProps) {
   return (
-    <div className='border-slate-900 border flex flex-col rounded overflow-hidden justify-between'>
-      <div>
-        <div className="flex w-full bg-hoodaLaranja py-2 px-4 items-center gap-4">
-          <h3 className='text-lg font-bold text-center uppercase '>{categoria.nome}</h3>
-        </div>
-        <div className='p-4 '>
-          <p>{categoria.descricao}</p>
+    // <div className='border-slate-900 border flex flex-col rounded overflow-hidden justify-between'>
+    //   <div>
+    //     <div className="flex w-full bg-hoodaLaranja py-2 px-4 items-center gap-4">
+    //       <h3 className='text-lg font-bold text-center uppercase '>{categoria.nome}</h3>
+    //     </div>
+    //     <div className='p-4 '>
+    //       <p>{categoria.descricao}</p>
           
           
-        </div>
-      </div>
-      <div className="flex">
-      <Link to={`/editarCategoria/${categoria.id}`} className='w-full text-white bg-hoodaLaranja hover:bg-amber-400 flex items-center justify-center py-2'>
-          <button>Editar</button>
-        </Link>
-        <Link to={`/delete/${categoria.id}`} className='text-white bg-red-400 hover:bg-red-700 w-full flex items-center justify-center'>
-          <button>Deletar</button>
-        </Link>
-      </div>
-    </div>
+    //     </div>
+    //   </div>
+    //   <div className="flex">
+    //   <Link to={`/editarCategoria/${categoria.id}`} className='w-full text-white bg-hoodaLaranja hover:bg-amber-400 flex items-center justify-center py-2'>
+    //       <button>Editar</button>
+    //     </Link>
+    //     <Link to={`/delete/${categoria.id}`} className='text-white bg-red-400 hover:bg-red-700 w-full flex items-center justify-center'>
+    //       <button>Deletar</button>
+    //     </Link>
+    //   </div>
+    // </div>
+
+    <div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+  <div className="p-6">
+  <h3 className='text-lg font-bold text-center uppercase m-7'>{categoria.nome}</h3>
+  <p>{categoria.descricao}</p>
+  </div>
+  <div className="p-6 pt-0 flex justify-evenly">
+  <Link to={`/editarCategoria/${categoria.id}`}>
+    <button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-hoodaLaranja text-white shadow-md shadow-inner hover:shadow-lg hover:bg-amber-400 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none m-4 "
+      type="button">
+     Editar
+    </button></Link>
+    <Link to={`/delete/${categoria.id}`}>
+    <button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-red-600 text-white shadow-md shadow-inner hover:shadow-lg hover:bg-amber-400 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none m-4"
+      type="button">
+     Deletar
+    </button></Link>
+  </div>
+</div>
   )
 }
 
--- src/Components/Categoria/deleteCategoria/DeleteCategoria.tsx
+++ src/Components/Categoria/deleteCategoria/DeleteCategoria.tsx
@@ -64,22 +64,45 @@ function DeleteCategoria() {
         retornar()
     }
     return (
-        <div className='container w-1/3 mx-auto'>
-            <h1 className='text-4xl text-center my-4'>Deletar categoria</h1>
+        // <div className='container w-1/3 mx-auto'>
+        //     <h1 className='text-4xl text-center my-4'>Deletar categoria</h1>
 
-            <p className='text-center font-semibold mb-4'>Você tem certeza de que deseja apagar o categoria a seguir?</p>
+        //     <p className='text-center font-semibold mb-4'>Você tem certeza de que deseja apagar o categoria a seguir?</p>
 
-            <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-                <header className='py-2 px-6 bg-hoodaLaranja text-white font-bold text-2xl'>categoria</header>
-                <p className='p-8 text-3xl bg-slate-200 h-full'>{categoria.descricao}</p>
-                <div className="flex">
-                    <button className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2' onClick={retornar}>Não</button>
-                    <button className='w-full text-slate-100 bg-hoodaLaranja hover:bg-amber-400 flex items-center justify-center' onClick={deletarCategorias}>
-                        Sim
-                    </button>
-                </div>
-            </div>
+        //     <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
+        //         <header className='py-2 px-6 bg-hoodaLaranja text-white font-bold text-2xl'>categoria</header>
+        //         <p className='p-8 text-3xl bg-slate-200 h-full'>{categoria.descricao}</p>
+        //         <div className="flex">
+        //             <button className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2' onClick={retornar}>Não</button>
+        //             <button className='w-full text-slate-100 bg-hoodaLaranja hover:bg-amber-400 flex items-center justify-center' onClick={deletarCategorias}>
+        //                 Sim
+        //             </button>
+        //         </div>
+        //     </div>
+        // </div>
+
+        <div className='container flex justify-center mt-20'>
+        <div className="relative flex flex-col justify-center text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+  <div className="p-6 ">
+  <h3 className='text-lg font-bold text-center uppercase m-7'>Deletar categoria {categoria.nome}?</h3>
+  <p>{categoria.descricao}</p>
+  </div>
+  <div className="p-6 pt-0 flex justify-evenly">
+  
+    <button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-hoodaLaranja text-white shadow-md shadow-inner hover:shadow-lg hover:bg-amber-400 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none m-4 "
+      type="button" onClick={retornar}>
+     Não
+    </button>
+     
+    <button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-red-600 text-white shadow-md shadow-inner hover:shadow-lg hover:bg-amber-400 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none m-4"
+      type="button" onClick={deletarCategorias}>
+     Sim
+    </button>
         </div>
+    </div>
+</div>
     )
 }
 
