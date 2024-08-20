c8227a72c1d1a124a74a6713a4b1a850cd406d08
Rebeca Ouriques <ouriques.rebecah@gmail.com>
2024-08-20 15:27:33 -0300

    Ajuste da home  e cardProdutos

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,31 +1,32 @@
-import axios from "axios"; 
+import axios from "axios";
 
 const api = axios.create({
-    baseURL:'https://hooda-api.onrender.com/'
+    baseURL: 'https://hooda-api.onrender.com/'
 })
 
-    export const cadastrarUsuario = async(url: string, dados: object, setDados: Function) => {
-        const resposta = await api.post(url, dados)
-            setDados(resposta.data)
+export const cadastrarUsuario = async (url: string, dados: object, setDados: Function) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
 }
 
-    export const login = async(url: string, dados: Object, setDados: Function) => {
-        const resposta = await api.post(url, dados)
-            setDados(resposta.data)
+export const login = async (url: string, dados: Object, setDados: Function) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
 }
-    export const buscar = async(url: string, setDados: Function, header: Object) => {
-        const resposta = await api.get(url, header)
-            setDados(resposta.data)
-}
-    export const deletar = async(url: string, header: Object) => {
-        await api.delete(url, header)
+export const buscar = async (url: string, setDados: Function, header?: Object) => {
+    const resposta = await api.get(url, header)
+    setDados(resposta.data)
 }
 
-     export const atualizar = async(url: string, dados: Object, setDados: Function, header: Object) => {
-     const resposta = await api.put(url, dados, header)
-     setDados(resposta.data)
+export const deletar = async (url: string, header: Object) => {
+    await api.delete(url, header)
 }
-export const cadastrar = async(url: string, dados: Object, setDados: Function, header: Object) => {
+
+export const atualizar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.put(url, dados, header)
+    setDados(resposta.data)
+}
+export const cadastrar = async (url: string, dados: Object, setDados: Function, header: Object) => {
     const resposta = await api.post(url, dados, header)
     setDados(resposta.data)
-  }
+}
