92a32e20a6f7e416b1ea76052b6debce35d22447
Rebeca Ouriques <ouriques.rebecah@gmail.com>
2024-08-20 11:02:36 -0300

    Feat: crud-produto

---
+++ src/Components/Produto/ListProduto/ListProduto.tsx
@@ -0,0 +1,67 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { DNA } from 'react-loader-spinner';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthContext';
+import { toastAlerta } from '../../../utils/ToastAlerta';
+import { buscar } from '../../../services/Service';
+import CardProduto from '../CardProduto/CardProduto';
+import Produto from '../../../model/Produto';
+
+function ListProduto () {
+
+    const [produtos, setProdutos] = useState<Produto[]>([]);
+
+    let navigate = useNavigate();
+  
+    const { usuario, handleLogout } = useContext(AuthContext);
+    const token = usuario.token;
+  
+    useEffect(() => {
+      if (token === '') {
+        toastAlerta('Você precisa estar logado', 'info');
+        navigate('/');
+      }
+    }, [token]);
+  
+    async function buscarProdutos() {
+      try {
+        await buscar('/produtos', setProdutos, {
+          headers: {
+            Authorization: token,
+          },
+        });
+      } catch (error: any) {
+        if (error.toString().includes('403')) {
+          toastAlerta('O token expirou, favor logar novamente', 'info')
+          handleLogout()
+        }
+      }
+    }
+  
+    useEffect(() => {
+      buscarProdutos();
+    }, [produtos.length]);
+  
+    return (
+      <>
+        {produtos.length === 0 && (
+          <DNA
+            visible={true}
+            height="200"
+            width="200"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper mx-auto"
+          />
+        )}
+        <div className='container mx-auto my-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+          {produtos.map((produto) => (
+            <CardProduto key={produto.id} produto={produto} />
+          ))}
+        </div>
+      </>
+    );
+  }
+  
+
+export default ListProduto
