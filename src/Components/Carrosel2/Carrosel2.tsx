21d120875d1b743d320a51be499ae2239afcb3ba
Rosdrigo <rosdrigol.tech@gmail.com>
2024-08-21 13:47:26 -0300

    add imgs

--- src/Components/Carrosel2/Carrosel2.tsx
+++ src/Components/Carrosel2/Carrosel2.tsx
@@ -13,7 +13,7 @@ export default function Carrosel2() {
             spaceBetween={30}
             centeredSlides={true}
             autoplay={{
-                delay: 2500,
+                delay: 4500,
                 disableOnInteraction: false,
             }}
             pagination={{
@@ -26,11 +26,24 @@ export default function Carrosel2() {
             <SwiperSlide className="slide">
                 <img src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhhTcLHPZ0P3g0cIF18yOVZOzcTMMpJheWX9XFPPwcO5iPH667i9Z0ieOKUgbcZnUhCJAjnLfduEwrgLBB9OU3QsvAoU685mg8pLhUHnGfzfgQtPssdL4ySl1cn7Ln4EdeXSfpNHN4lLyaG/s1600/folhas-em-tons-amarelo-laranja-vermelho-e-verde-imagens-imagem-de-fundo-wallpaper-para-pc-computador-tela-gratis-ambiente-de-trabalho.jpg' alt="" />
                 <div className="text-overlay">
-                    <h1 className="overlay-text">Texto Grande e Centralizado</h1>
-                    <Link to={'https://github.com/rosdrigo'} className="slide-button">CLIQUE AQUI</Link>
+                    <h1 className="overlay-text font-Montserrat">O planeta agradece seu esforço.</h1>
+                    <Link to={'https://github.com/rosdrigo'} className="slide-button">SAIBA+</Link>
                 </div>
             </SwiperSlide>
-            {/* Adicione mais SwiperSlides conforme necessário */}
+            <SwiperSlide className="slide">
+                <img src='https://i.imgur.com/IaXaXVK.png' alt="" />
+                <div className="text-overlay">
+                    <h1 className="overlay-text font-Montserrat">Cada gesto conta na preservação.</h1>
+                    <Link to={'https://github.com/rosdrigo'} className="slide-button">SAIBA+</Link>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className="slide">
+                <img src='https://i.imgur.com/jYNhgmE.png' alt="" />
+                <div className="text-overlay">
+                    <h1 className="overlay-text font-Montserrat">A natureza não pode esperar.</h1>
+                    <Link to={'https://github.com/rosdrigo'} className="slide-button">SAIBA+</Link>
+                </div>
+            </SwiperSlide>
         </Swiper>
     );
 }
