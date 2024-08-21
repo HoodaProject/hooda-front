57b502710c18cadcd95ce040736aecae9e136c4f
Alexander Pereira <xandi9731@gmail.com>
2024-08-21 11:08:34 -0300

    Style: Estilização e responsividade do footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -4,35 +4,33 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <>
-      <footer className="bg-white">
-        <div className="container px-4 mx-auto">
+      <footer className="bg-[#342E09] mt-10">
+        <div className="container px-4 mx-auto flex justify-center">
           <div className="pt-[2rem] pb-[2rem] mx-auto max-w-4xl">
             <img
               src="https://i.imgur.com/XlH3SL3.png"
               alt="Logo"
-              className="h-32 block md:mx-auto"
+              className="h-[6rem] block md:mx-auto"
             />
 
-            <div className="flex flex-wrap justify-center -mx-3 lg:-mx-6">
+            <div className="flex grid grid-cols-1 mx-auto md:grid-cols-3 lg:grid-cols-5 -mx-3 lg:-mx-6">
               <div className="w-full md:w-auto p-3 md:px-6">
                 
-                  <Link className="text-lg text-gray-500 hover:text-amber-400 hover:text-xl duration-200 font-medium" to="/home">Home</Link>
+                  <Link className="text-lg text-amber-400 hover:text-white hover:text-xl duration-200 font-medium" to="/home">Home</Link>
               </div>
               <div className="w-full md:w-auto p-3 md:px-6">
-                
-                  <Link className="text-lg text-gray-500 hover:text-amber-400 hover:text-xl duration-200 font-medium" to="/aboutus">Sobre</Link>
-                
-              </div>
-              <div className="w-full md:w-auto p-3 md:px-6">
-                  <Link to={'#'} className="text-lg text-gray-500 hover:text-amber-400 hover:text-xl duration-200 font-medium" >Projeto</Link>
-              </div>
-              <div className="w-full md:w-auto p-3 md:px-6">
-                
-                 <Link to={'#'} className="text-lg text-gray-500 hover:text-amber-400 hover:text-xl duration-200 font-medium"> Aguarde...</Link>
+                  <Link className="text-lg text-amber-400 hover:text-white hover:text-xl duration-200 font-medium" to="/aboutus">Sobre</Link>
                 
               </div>
               <div className="w-full md:w-auto p-3 md:px-6">
-                 <Link to={'#'} className="text-lg text-gray-500 hover:text-amber-400 hover:text-xl duration-200 font-medium"> Aguarde...</Link>
+                  <Link to={'#'} className="text-lg text-amber-400 hover:text-white hover:text-xl duration-200 font-medium" >Projeto</Link>
+              </div>
+              <div className="w-full md:w-auto p-3 md:px-6">
+                 <Link to={'#'} className="text-lg text-amber-400 hover:text-white hover:text-xl duration-200 font-medium"> Home</Link>
+                
+              </div>
+              <div className="w-full md:w-auto p-3 md:px-6">
+                 <Link to={'#'} className="text-lg text-amber-400 hover:text-white hover:text-xl duration-200 font-medium"> Home</Link>
               </div>
             </div>
           </div>
