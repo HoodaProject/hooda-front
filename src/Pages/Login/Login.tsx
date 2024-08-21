9153e2c1f4723f700114e8314b3f04f0cbe3b06d
Camila Andrade <camila.rda@outlook.com>
2024-08-21 16:51:12 -0300

    add component perfil

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -49,11 +49,8 @@ function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
               <div>
                 <label className="text-gray-800 text-xs block mb-2">Email</label>
                 <div className="relative flex items-center">
-<<<<<<< HEAD
+
                   <input name="usuario" value={usuarioLogin.usuario} type="text" required className="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none" placeholder="Seu email" 
-=======
-                  <input name="usuario" type="text" required className="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none" placeholder="Seu email" value={usuarioLogin.usuario}
->>>>>>> f5cfe173289674454db12b0201c12bd6f216b3a9
                     onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)} />
                   <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-[18px] h-[18px] absolute right-2" viewBox="0 0 682.667 682.667">
                     <defs>
@@ -72,12 +69,8 @@ function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
               <div className="mt-8">
                 <label className="text-gray-800 text-xs block mb-2">Senha</label>
                 <div className="relative flex items-center">
-<<<<<<< HEAD
                   <input name="senha" value={usuarioLogin.senha} type="password" required className="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none" placeholder="Sua senha" 
-=======
-                  <input name="senha" type="password" required className="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none" placeholder="Sua senha" value={usuarioLogin.senha}
->>>>>>> f5cfe173289674454db12b0201c12bd6f216b3a9
-                    onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)} />
+                  onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)} />
                   <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-[18px] h-[18px] absolute right-2 cursor-pointer" viewBox="0 0 128 128">
                     <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
                   </svg>
