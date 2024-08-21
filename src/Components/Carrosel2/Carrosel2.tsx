558e420da97199c4cb9a077548185dbf198afeee
Rosdrigo <rosdrigol.tech@gmail.com>
2024-08-21 15:12:18 -0300

    carrosel imgs

--- src/Components/Carrosel2/Carrosel2.tsx
+++ src/Components/Carrosel2/Carrosel2.tsx
@@ -26,21 +26,28 @@ export default function Carrosel2() {
             <SwiperSlide className="slide">
                 <img src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhhTcLHPZ0P3g0cIF18yOVZOzcTMMpJheWX9XFPPwcO5iPH667i9Z0ieOKUgbcZnUhCJAjnLfduEwrgLBB9OU3QsvAoU685mg8pLhUHnGfzfgQtPssdL4ySl1cn7Ln4EdeXSfpNHN4lLyaG/s1600/folhas-em-tons-amarelo-laranja-vermelho-e-verde-imagens-imagem-de-fundo-wallpaper-para-pc-computador-tela-gratis-ambiente-de-trabalho.jpg' alt="" />
                 <div className="text-overlay">
-                    <h1 className="overlay-text font-Montserrat">O planeta agradece seu esforço.</h1>
+                    <h1 className="overlay-text font-Montserrat">O <span className='text-green-700 under'>planeta</span> agradece seu esforço.</h1>
                     <Link to={'https://github.com/rosdrigo'} className="slide-button">SAIBA+</Link>
                 </div>
             </SwiperSlide>
             <SwiperSlide className="slide">
                 <img src='https://i.imgur.com/IaXaXVK.png' alt="" />
                 <div className="text-overlay">
-                    <h1 className="overlay-text font-Montserrat">Cada gesto conta na preservação.</h1>
+                    <h1 className="overlay-text font-Montserrat font-semibold">Cada gesto conta na <span className='text-hoodaLaranja'>preservação.</span></h1>
                     <Link to={'https://github.com/rosdrigo'} className="slide-button">SAIBA+</Link>
                 </div>
             </SwiperSlide>
             <SwiperSlide className="slide">
-                <img src='https://i.imgur.com/jYNhgmE.png' alt="" />
+                <img src='https://i.imgur.com/uepejry.jpeg' alt="" />
                 <div className="text-overlay">
-                    <h1 className="overlay-text font-Montserrat">A natureza não pode esperar.</h1>
+                    <h1 className="overlay-text font-Montserrat">A natureza <span className='text-red-500'>não</span> pode esperar.</h1>
+                    <Link to={'https://github.com/rosdrigo'} className="slide-button">SAIBA+</Link>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className="slide">
+                <img src='https://i.imgur.com/HLTSpln.jpeg' alt="" />
+                <div className="text-overlay">
+                    <h1 className="overlay-text font-Montserrat"><span className='text-blue-400'>Recicle</span> para um futuro melhor.</h1>
                     <Link to={'https://github.com/rosdrigo'} className="slide-button">SAIBA+</Link>
                 </div>
             </SwiperSlide>
