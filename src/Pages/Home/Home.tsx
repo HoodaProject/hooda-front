1f235829d390ac26d1ed1f55b00c6657bf59e35f
Rodrigo <rosdrigol@icloud.com>
2024-08-22 14:39:25 -0300

    add home menu styles

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,64 +1,63 @@
-//import Carousel from '../../Components/Carrosel/Carrosel'
-import { useEffect, useState } from 'react';
-import Carrosel2 from '../../Components/Carrosel/Carrosel'
-import ListProduto from '../../Components/Produto/ListProduto/ListProduto'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Carrosel2 from '../../Components/Carrosel/Carrosel';
+import ListProduto from '../../Components/Produto/ListProduto/ListProduto';
 import Categoria from '../../model/Categoria';
 import { buscar } from '../../services/Service';
-import { Link } from 'react-router-dom';
-
 
 function Home() {
+    const [categorias, setCategorias] = useState<Categoria[]>([]);
+    const [showList, setShowList] = useState(false);
 
-        const [categorias, setCategorias] = useState<Categoria[]>([]);
-        async function buscarCategorias() {
-                await buscar('/categorias', setCategorias);
-        }
+    async function buscarCategorias() {
+        await buscar('/categorias', setCategorias);
+    }
 
-        useEffect(() => {
-                buscarCategorias();
-        }, [categorias.length]);
+    useEffect(() => {
+        buscarCategorias();
+    }, [categorias.length]);
 
-        
-        return (
+    const toggleList = () => {
+        setShowList(!showList);
+    };
 
-                <div className='grid grid-cols-12'>
-                        <div className='min-h-[0vh] max-h-[60vh] max-md:hidden pb-8 flex items-center col-span-12 p-0 m-0'>
-                                <Carrosel2 />
-                        </div>
-                        <div className='text-hoodaLaranja xl:text-3xl text-center col-span-2 text-md md:text-2xl lg:text-3xl'>
-                                Categorias
-                                <ul className='text-black text-2xl p-2 m-2'>
-                                        {categorias.map(categoria => (
-                                                <li key={categoria.id} className='p-1 m-1 hover:text-hoodaLaranja hover:underline flex justify-center text-sm  md:text-md lg:text-2xl '>
-                                                        <Link to={`/categorias/${categoria.id}`}>
-                                                                {categoria.nome}
-                                                        </Link>
-                                                </li>
-                                        ))}
-
-                                </ul>
-                        </div>
-
-                        <div className='col-span-10 flex justify-center'>
-                                <div>
-                                        <div className='flex flex-row flex-wrap justify-evenly py-0 px-0'>
-
-                                                <ListProduto />
-
-                                        </div>
-                                        <p className='font-Docker-One text-hoodaLaranja flex justify-center'>Paginas</p>
-
-                                        <ul className='flex justify-center'>
-                                                <li className=' hover:text-hoodaLaranja hover:underline p-1'> 1 </li>
-                                                <li className=' hover:text-hoodaLaranja hover:underline p-1'> 2 </li>
-                                                <li className=' hover:text-hoodaLaranja hover:underline p-1'> 3 </li>
-                                                <li className=' hover:text-hoodaLaranja hover:underline p-1'> 4 </li>
-                                        </ul>
-                                </div>
-                        </div>
-
+    return (
+        <div className='grid grid-cols-12'>
+            <div className='min-h-[0vh] max-h-[60vh] max-md:hidden pb-8 flex items-center col-span-12 p-0 m-0'>
+                <Carrosel2 />
+            </div>
+            <div className='text-hoodaLaranja xl:text-3xl text-center col-span-2 text-md md:text-2xl lg:text-3xl'>
+                <button onClick={toggleList} className='bg-hoodaLaranja text-white p-2 rounded'>
+                    Categorias
+                </button>
+                {showList && (
+                    <ul className='text-black text-2xl p-2 m-2'>
+                        {categorias.map(categoria => (
+                            <li key={categoria.id} className='p-1 m-1 hover:text-hoodaLaranja hover:underline flex justify-center text-sm md:text-md lg:text-2xl'>
+                                <Link to={`/categorias/${categoria.id}`}>
+                                    {categoria.nome}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+            <div className='col-span-10 flex justify-center'>
+                <div>
+                    <div className='flex flex-row flex-wrap justify-evenly py-0 px-0'>
+                        <ListProduto />
+                    </div>
+                    <p className='font-Docker-One text-hoodaLaranja flex justify-center'>Paginas</p>
+                    <ul className='flex justify-center'>
+                        <li className='hover:text-hoodaLaranja hover:underline p-1'>1</li>
+                        <li className='hover:text-hoodaLaranja hover:underline p-1'>2</li>
+                        <li className='hover:text-hoodaLaranja hover:underline p-1'>3</li>
+                        <li className='hover:text-hoodaLaranja hover:underline p-1'>4</li>
+                    </ul>
                 </div>
-        )
+            </div>
+        </div>
+    );
 }
 
-export default Home
+export default Home;
