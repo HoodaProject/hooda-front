1fe6c40b5e126c8620d04d61e47a9435b5ca4cf6
Rodrigo <rosdrigol@icloud.com>
2024-08-23 10:14:11 -0300

    version 1.1

--- src/Components/Produto/ProdutoCategoria/ProdutoCategoria.tsx
+++ src/Components/Produto/ProdutoCategoria/ProdutoCategoria.tsx
@@ -5,9 +5,7 @@ import Categoria from '../../../model/Categoria';
 import { AuthContext } from '../../../contexts/AuthContext';
 import { toastAlerta } from '../../../utils/ToastAlerta';
 
-
 function ProdutosPorCategoria() {
-
     const { usuario } = useContext(AuthContext);
     const token = usuario.token;
 
@@ -29,9 +27,8 @@ function ProdutosPorCategoria() {
                 },
             });
         } catch (error: any) {
-            toastAlerta("Erro ao encotrar categoria", "erro")
+            toastAlerta("Erro ao encontrar categoria", "erro");
         }
-
     }
 
     useEffect(() => {
@@ -48,28 +45,29 @@ function ProdutosPorCategoria() {
     }, [id]);
 
     return (
-        <div className='flex flex-row flex-wrap justify-evenly py-0 px-0'>
-            <h1 className='text-hoodaLaranja text-xl py-5 flex justify-center'>Produtos da Categoria {categoria.nome}</h1>
-            <div className='container mx-auto my-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3'>
-
-                {categoria.produto?.map(item => (
-                    <div key={item.id} className=''>
-                        <div className="w-60 bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden m-2">
-                            <div className="relative h-40">
-                                <img src={item.imagem} className="h-full w-full object-cover" alt="" />
-                            </div>
-                            <div className='p-4'>
-                                <div className="mb-2 flex items-center justify-between text-xl">
-                                    <h2 className='text-hoodaLaranja font-medium'>{item.nome}</h2>
+        <div className='flex flex-col items-center pt-4'>
+            <div className='w-full bg-gray-200 max-w-6xl p-4 m-4 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md'>
+                <h1 className='text-hoodaLaranja font-Montserrat text-2xl py-5 text-center'> {categoria.nome}</h1>
+                <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3'>
+                    {categoria.produto?.map(item => (
+                        <div key={item.id} className=''>
+                            <div className="w-60 bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden m-2">
+                                <div className="relative h-40">
+                                    <img src={item.imagem} className="h-full w-full object-cover" alt="" />
                                 </div>
-                                <div className="mb-2 flex items-center justify-between text-md">
-                                    <p className="text-hoodaLaranja font-medium">Preço: R${item.preco}</p>
+                                <div className='p-4'>
+                                    <div className="mb-2 flex items-center justify-between text-xl">
+                                        <h2 className='text-hoodaLaranja font-medium'>{item.nome}</h2>
+                                    </div>
+                                    <div className="mb-2 flex items-center justify-between text-md">
+                                        <p className="text-hoodaLaranja font-medium">Preço: R${item.preco}</p>
+                                    </div>
+                                    <p className="text-gray-600 text-sm font-normal opacity-75">Descrição: {item.descricao}</p>
                                 </div>
-                                <p className="text-gray-600 text-sm font-normal opacity-75">Descrição: {item.descricao}</p>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
     );
